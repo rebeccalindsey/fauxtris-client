a4dc218b1fa5a51847eb46c1df6187cfb7fe028d
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-11 12:31:16 -0500

    Refactor populateBoard with createNewBoard

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,12 +1,21 @@
-function populateBoard() {
-    // 20 high, 10 across
+function createNewBoard() {
     let gameRows = {}
-    for (let i = 0; i < 20; i++){
-        gameRows[`${String.fromCharCode(97 + i)}Row`] = Array(10).fill(null)
-        gameRows[Object.keys(gameRows)[i]].forEach((space) => {
+    for (let i = 0; i < 20; i++) {
+        const rowName = `${String.fromCharCode(97 + i)}Row`
+        gameRows[rowName] = Array(10).fill(null)
+    }
+    populateBoard(gameRows)
+}
+
+function populateBoard(board) {
+    for (const row in board) {
+        for (const space of board[`${row}`]){
             const newSpace = document.createElement("div")
             newSpace.classList.add("game-space")
-            document.getElementById("game-display").append(newSpace)
-        })
+            if (space != null) {
+                // run new function here
+            }
+                document.getElementById("game-display").append(newSpace)
+        }
     }
 }
