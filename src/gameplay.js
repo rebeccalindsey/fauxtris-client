3c3d29d036f4c5043511d46787c1fa0fe4993226
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-11 16:04:39 -0500

    Refactor gameplay into class with static functions

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,35 +1,45 @@
-function createNewBoard() {
-    let gameRows = {}
-    for (let i = 0; i < 20; i++) {
-        const rowName = `${String.fromCharCode(97 + i)}Row`
-        gameRows[rowName] = Array(10).fill(null)
-    }
-    populateBoard(gameRows)
-}
+class Gameplay {
 
-function populateBoard(board) {
-    for (const row in board) {
-        for (const space of board[`${row}`]){
-            const newSpace = document.createElement("div")
-            newSpace.classList.add("game-space")
-            if (space != null) {
-                newSpace.classList.add(addColor(space))
-                console.log(addColor(space))
+    static gameBoard = {}
+
+    static score = 0
+
+    static createNewBoard() {
+        for (let i = 20; i > 0; i--) {
+            const rowName = `${String.fromCharCode(96 + i)}Row`
+            Gameplay.gameBoard[rowName] = Array(10).fill(null)
+        }
+        Gameplay.populateBoard()
+    }
+
+    static clearBoard() {
+        document.getElementById("game-display").innerHTML = ''
+    }
+
+    static populateBoard() {
+        Gameplay.clearBoard()
+        for (const row in Gameplay.gameBoard) {
+            for (const space of Gameplay.gameBoard[`${row}`]){
+                const newSpace = document.createElement("div")
+                newSpace.classList.add("game-space")
+                if (space != null) {
+                    newSpace.classList.add(Gameplay.addColor(space))
+                }
+                    document.getElementById("game-display").append(newSpace)
             }
-                document.getElementById("game-display").append(newSpace)
         }
     }
-}
 
-function addColor(space) {
-    const colorList = {
-        "I": "i-block",
-        "O": "o-block",
-        "T": "t-block",
-        "S": "s-block",
-        "Z": "z-block",
-        "J": "j-block",
-        "L": "l-block",
+    static addColor(space) {
+        const colorList = {
+            "I": "i-block",
+            "O": "o-block",
+            "T": "t-block",
+            "S": "s-block",
+            "Z": "z-block",
+            "J": "j-block",
+            "L": "l-block",
+        }
+        return colorList[space]
     }
-    return colorList[space]
 }
