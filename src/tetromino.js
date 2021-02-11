b280a04af870679725a346861c94418fa1b149c1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-11 16:48:14 -0500

    Add clearBoard method and refactor iBlock class

--- src/tetromino.js
+++ src/tetromino.js
@@ -1,16 +1,28 @@
 class Tetromino {
-    constructor() {
-        this.element = document.createElement("div")
-        this.element.classList.add("tetromino")
-    }
+    // constructor() {
+    //     this.element = document.createElement("div")
+    //     this.element.classList.add("tetromino")
+    // }
 
 }
 
 class iBlock extends Tetromino {
-    constructor(element) {
-        super(element)
-        this.element.classList.add("i-block")
-        document.getElementById("game-display").appendChild(this.element)
+    // constructor(element) {
+    //     super(element)
+    //     this.element.classList.add("i-block")
+    //     document.getElementById("game-display").appendChild(this.element)
+    // }
+    constructor() {
+        super()
+        this.active = true
+        this.populateIBlock()
+    }
+
+    populateIBlock() {
+        for (let i = 3; i < 7; i++) {
+            Gameplay.gameBoard["tRow"][i] = "I"
+        }
+        Gameplay.populateBoard()
     }
 
 }
