805762b13d5898f7b4bba43a93496cabfa308991
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-17 12:04:28 -0500

    Refactor oBlock#populateOBlock and constructor

--- src/oblock.js
+++ src/oblock.js
@@ -1,16 +1,29 @@
 class oBlock extends Tetromino {
   constructor(orientation) {
     super(orientation);
-    this.active = true;
-    Tetromino.activeBlock = this;
-    this.populateOBlock();
+    this.activeBlocks = this.populateOBlock();
   }
 
   populateOBlock() {
+    const blocksToAdd = {};
+    const firstRowName = Object.keys(Gameplay.gameBoard)[0];
+    const secondRowName = Object.keys(Gameplay.gameBoard)[1];
+
     for (let i = 4; i < 6; i++) {
-      Gameplay.gameBoard["tRow"][i] = "oBlock";
-      Gameplay.gameBoard["sRow"][i] = "oBlock";
+      if (blocksToAdd.hasOwnProperty(firstRowName)) {
+        blocksToAdd[firstRowName].push(i);
+      } else {
+        blocksToAdd[firstRowName] = [i];
+      }
+
+      if (blocksToAdd.hasOwnProperty(secondRowName)) {
+        blocksToAdd[secondRowName].push(i);
+      } else {
+        blocksToAdd[secondRowName] = [i];
+      }
     }
-    Gameplay.populateBoard();
+
+    this.updateBlocks(blocksToAdd);
+    return blocksToAdd;
   }
 }
