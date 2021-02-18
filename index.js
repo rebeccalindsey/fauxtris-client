d39c11a344c2918f3fb0be15c4e4149a5fc02845
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-18 12:31:07 -0500

    Set up click event listener

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  Gameplay.createNewBoard();
+  //   Gameplay.createNewBoard();
   document
     .getElementById("game-display")
     .addEventListener("keydown", function (event) {
       Gameplay.handleArrowKey(event.key);
     });
+
+  document
+    .getElementById("horizontal-content")
+    .addEventListener("click", function (event) {
+      handleClick(event);
+    });
 });
+
+function handleClick(event) {
+  switch (event.target.id) {
+    case "new-game":
+      console.log("new game");
+      break;
+    case "instructions":
+      console.log("instructions");
+      break;
+    case "leaderboard":
+      console.log("leaderboard");
+      break;
+    case "play":
+      console.log("play");
+      break;
+  }
+}
